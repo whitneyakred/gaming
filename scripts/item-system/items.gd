8ae2faacc0eb8a616bb0884a03e45583e032c0fb
IvanRL25 <146905269+IvanRL25@users.noreply.github.com>
2026-09-17 22:01:13 -0500

    creation of basic item system that loads during game start up

---
+++ scripts/item-system/items.gd
@@ -0,0 +1,31 @@
+extends Node
+
+func _ready() -> void:
+	ItemDatabase.register_item(make_cod())
+	ItemDatabase.register_item(make_rope())
+	ItemDatabase.register_item(make_fishing_rod())
+
+func make_cod() -> ItemData:
+	var item := ItemData.new()
+	item.id = &"fish_cod"
+	item.display_name = "Cod"
+	item.description = "A common ocean fish."
+	item.max_stack = 10
+	return item
+
+func make_rope() -> ItemData:
+	var item := ItemData.new()
+	item.id = &"rope"
+	item.display_name = "Rope"
+	item.description = "Useful for ship repairs."
+	item.max_stack = 25
+	return item
+
+func make_fishing_rod() -> ItemData:
+	var item := ItemData.new()
+	item.id = &"fishing_rod"
+	item.display_name = "Fishing Rod"
+	item.description = "An old but useful tool for getting food and other items from the
+	sea."
+	item.max_stack = 1
+	return item
