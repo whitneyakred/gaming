90b9ca91c8068e7c22eb9c9ddbc9ad8e764f632c
IvanRL25 <146905269+IvanRL25@users.noreply.github.com>
2026-09-18 10:25:20 -0500

    updated supporting files to match with updated item-system branch

--- scripts/item-system/items.gd
+++ scripts/item-system/items.gd
@@ -1,30 +1,33 @@
 extends Node
 
 func _ready() -> void:
-	ItemDatabase.register_item(make_cod())
-	ItemDatabase.register_item(make_rope())
-	ItemDatabase.register_item(make_fishing_rod())
+	ItemDatabase.register_item(make_item(
+		&"fish_cod",
+		"Cod",
+		"A common ocean fish.",
+		10
+	))
+	ItemDatabase.register_item(make_item(
+		&"rope",
+		"Rope",
+		"Useful for ship repairs.",
+		25
+	))
+	ItemDatabase.register_item(make_item(
+		&"fishing_rod",
+		"Fishing Rod",
+		"An old but useful tool for getting food and other items from the sea."
+	))
 
-func make_cod() -> ItemData:
+func make_item(
+	item_id: StringName,
+	item_display_name: String,
+	item_description: String,
+	item_max_stack: int = 1
+) -> ItemData:
 	var item := ItemData.new()
-	item.id = &"fish_cod"
-	item.display_name = "Cod"
-	item.description = "A common ocean fish."
-	item.max_stack = 10
-	return item
-
-func make_rope() -> ItemData:
-	var item := ItemData.new()
-	item.id = &"rope"
-	item.display_name = "Rope"
-	item.description = "Useful for ship repairs."
-	item.max_stack = 25
-	return item
-
-func make_fishing_rod() -> ItemData:
-	var item := ItemData.new()
-	item.id = &"fishing_rod"
-	item.display_name = "Fishing Rod"
-	item.description = "An old but useful tool for getting food and other items from the sea."
-	item.max_stack = 1
+	item.id = item_id
+	item.display_name = item_display_name
+	item.description = item_description
+	item.max_stack = item_max_stack
 	return item
